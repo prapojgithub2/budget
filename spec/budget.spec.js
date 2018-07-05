c6d26e8d4fbabdccb29e231ae0fdffede77c348b
prapoj <prapoj@set.or.th>
2018-07-05 18:14:59 +0700

    done

--- spec/budget.spec.js
+++ spec/budget.spec.js
@@ -5,23 +5,44 @@ var Budget = require('../src/budget');
 describe("Query budget", () => {
     it("should return 0 when no budget in any month", () => {
         let existingBudgets = [];
+        budgetList=  existingBudgets
         let budget = Budget.queryBudget("01/01/2018", "02/01/2018");
         expect(budget).toEqual(0);
     });
 
     it("should return 100 baht when there query budget between 10/06/2018 to 10/06/2018 and there is 3000 baht budget in June 2018", () => {
+        let existingBudgets = [{
+            "month" : "06/2018",
+            "amount": 3000
+        }];
+        budgetList=  existingBudgets
         let budget = Budget.queryBudget("10/06/2018", "10/06/2018");
         
         expect(budget).toEqual(100);
     });
 
     it("should return 200 baht when there query budget between 10/06/2018 to 11/06/2018 and there is 3000 baht budget in June 2018", () => {
+        let existingBudgets = [{
+            "month" : "06/2018",
+            "amount": 3000
+        }];
+        budgetList=  existingBudgets
         let budget = Budget.queryBudget("10/06/2018", "11/06/2018");
         
         expect(budget).toEqual(200);
     });
 
     it("should return 5000 baht when there query budget between 01/06/2018 to 31/07/2018 and there is 3000 baht budget in June and 2000 in July", () => {
+        let existingBudgets = [{
+            "month" : "06/2018",
+            "amount": 3000
+        },
+        {
+            "month" : "07/2018",
+            "amount": 2000
+        }];
+        budgetList=  existingBudgets
+
         let budget = Budget.queryBudget("01/06/2018", "31/07/2018");
         
         expect(budget).toEqual(5000);
--- src/budget.js
+++ src/budget.js
@@ -3,38 +3,41 @@ var moment = require('moment');
 exports.queryBudget = function (startDate, endDate) {
     let sd = moment(startDate, "DD/MM/YYYY");
     let ed = moment(endDate, "DD/MM/YYYY");
-    let totalAmount = 0;
-    const startKey = `0${sd.month() + 1}/${sd.year()}`
-    const endKey = `0${ed.month() + 1}/${ed.year()}`
-    const numbersOfMonths = sd.month() - ed.month();
-    if(numbersOfMonths === 0) {
-        const daysInMonth = sd.daysInMonth();
-        let amount = 0;
-        for (let i = 0; i < budgetList.length; i++) {
-            const budget = budgetList[i];
-            if (startKey === budget.month) {
-                amount = budget.amount;
-                let duration = (ed.dayOfYear() - sd.dayOfYear())+1
-                
-                totalAmount = amount/daysInMonth*duration;
-            }
-        }
-    } else if( numbersOfMonths > 0) {
-        for (let i = 0; i < budgetList.length; i++) {
-            const budget = budgetList[i];
-            if (startKey === budget.month || endKey === budget.month) {
-                amount = budget.amount;
-                let duration = (ed.dayOfYear() - sd.dayOfYear())+1
-                
-                totalAmount += amount/daysInMonth*duration;
-            }
-        }
-    } else {
-        // error
-    }
 
-   
-    // console.log(sd.daysInMonth());
+    let startPeriod = moment(startDate, "DD/MM/YYYY").startOf('month')
+    let endPeriod = moment(endDate, "DD/MM/YYYY").endOf('month')
+
+
+    let totalAmount = 0
+
+    console.log(budgetList);
+    for (let i = 0; i < budgetList.length; i++) {
+        let budget = budgetList[i]
+        let thisMonth = moment(budget.month,"MM/YYYY")
+        console.log("budget list "  ,thisMonth)
+        if ( startPeriod <= thisMonth || endPeriod >= thisMonth) {
+            let sdm = moment(budget.month,"MM/YYYY").startOf('month')
+            let edm = moment(budget.month,"MM/YYYY").endOf('month')   
+            
+            let totalDays = edm.dayOfYear() - sdm.dayOfYear()+1
+            
+            let begin,end;
+            
+            if ( sd > sdm ) {
+                begin = sd
+            } else {
+                begin = sdm
+            }
+            if ( ed < edm ) {
+                end = ed
+            } else {
+                end = edm
+            }
+            
+            totalAmount += budget.amount *((end.dayOfYear() - begin.dayOfYear()) + 1 ) / totalDays
+        }
+
+    }
     return totalAmount;
 }
 
@@ -42,10 +45,4 @@ function getAmountByMonth(month){
     
 }
 
-var budgetList = [{
-    month: "06/2018",
-    amount: 3000
-},{
-    month: "07/2018",
-    amount: 2000
-}];
+budgetList = [];
